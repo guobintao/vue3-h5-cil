621adf89e87170525a98d4cfddb9ad73bc4818a5
guobintao <3057755982@qq.com>
2023-07-02 21:16:36 +0800

    列表抽离组件

--- dome/src/views/home/IndexView.vue
+++ dome/src/views/home/IndexView.vue
@@ -1,60 +1,19 @@
 <template>
   <div>
     <Swiper></Swiper>
-    <van-list v-model:loading="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <van-card v-for="item in productList" :key="item.pid" :price="item.sale_price" :desc="item.desc" :title="item.pname" :thumb="item.imgUrl">
-        <template #tags>
-          <van-tag plain type="primary">原价:{{ item.original_price }}</van-tag>
-          <van-tag plain type="primary">销量:{{ item.sales }}</van-tag>
-        </template>
-      </van-card>
-    </van-list>
+    <ShopList></ShopList>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import { useHomeSerivice } from '@/api/home'
-import { HomeManageType } from '@/interface/model/home'
+import ShopList from '@/components/ShopList.vue'
 import Swiper from '@/components/Swiper.vue'
 export default defineComponent({
   name: 'IndexView',
   components: {
-    Swiper
-  },
-  setup() {
-    const homeserivice = useHomeSerivice()
-    const state = {
-      loading: ref(false),
-      finished: ref(false),
-      error: ref(false),
-      productList: ref<Array<HomeManageType.ProductInterface>>([]),
-      pagesize: ref<number>(10),
-      pagecount: ref<number>(1)
-    }
-    const onLoad = async () => {
-      state.loading.value = true
-      const result = await homeserivice
-        .getGoodsList({
-          pagesize: state.pagesize.value,
-          pagecont: state.pagecount.value
-        })
-        .catch(() => {
-          state.error.value = true
-        })
-      if (result.code == 1) {
-        state.pagecount.value = state.pagecount.value + 1
-        state.productList.value = [...state.productList.value, ...result.result]
-      } else {
-        state.finished.value = true
-      }
-      state.loading.value = false
-    }
-
-    return {
-      ...state,
-      onLoad
-    }
+    Swiper,
+    ShopList
   }
 })
 </script>
